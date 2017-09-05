385c53e23fba0849bb180babee167d62b392e98b
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2017-09-05 23:14:35 +0600

    Create number.c

---
+++ number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+ int n;
+ printf("enter the number");
+ scanf("%d",&n);
+ if(n%2==0)
+ { 
+ printf("it is even number");
+ }
+ else
+ {
+ printf("it is odd number");
+ }
+ getch();
+ }
