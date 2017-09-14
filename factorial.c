fbcc79e5e8ee88b79ee1feeae5b329980af13c32
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2017-09-14 21:44:57 +0600

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+ int n;
+ printf("enter the number:");
+ scanf("%d",&n);
+ for(i=1;i<=n;){
+ i++;
+ sum=sum*i;
+ }
+ printf("the sum of factorial is %d",sum);
+ return 0;
+ }
+ 
+ 
