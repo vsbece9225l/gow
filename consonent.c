cf5d0c54050aa18f27db9209c36936bac63c1c25
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2017-09-14 21:25:50 +0600

    Create consonent.c

---
+++ consonent.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+ char ch;
+ printf("enter the character");
+ scanf("%c",ch);
+ int lowercasevowel,uppercasevowel;
+ lowercasevowel=(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+ uppercasevowel=(ch=='A'||ch=='E'||ch==I||ch=='O'||ch=='U')
+ if(lowercasevowel||uppercasevowel)
+ {
+ printf("%c is a vowel",c);
+ }
+ else
+ {
+  printf("is consonent %c",c);
+  }
+  return 0;
+  }
+  
