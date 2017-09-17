d4ecdf05d1f83f4c94b151ae4160d689993fbbaa
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2017-09-17 18:59:45 +0600

    Create armstrongint.c

---
+++ armstrongint.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+ int low,high,i,temp1,temp2,remainder,n=0,result=0;
+ printf("enter the two number");
+ scanf("%d%d",&low,&high);
+ printf("armstrong number between %dand%d are:",low,high);
+ for(i=low+1;i<high;++i)
+ {
+  temp2=i;
+  temp1=i;
+  while(temp!=0)
+  {
+   temp/=10;
+  ++n;
+  }
+  while(temp2!=0)
+  {
+   remainder=temp2%10;
+   result=result+pow(remainder,n)
+   }
+   temp2/=10;
+   }
+   printf("%d',i);
+   n=0;
+   result=0;
+   }
+   return 0;
+   }
+   
