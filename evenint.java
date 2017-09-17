db2af52afb8615ec21667bd2440deff809c1f0fc
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2017-09-17 09:57:13 +0600

    Create evenint.java

---
+++ evenint.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.scannar;
+class evenint
+{
+ public static void main(String args[])
+ {
+  int n,i;
+  system.out.println("enter the range :");
+  scannar sc= new scannar(system.in);
+  n= sc.nextInt();
+  for(i=0;i<n;i++)
+  {
+   if(i%2==0)
+   {
+    printf(" the number is prime %d",i);
+    }
+    }
+   }
+   }
+   
