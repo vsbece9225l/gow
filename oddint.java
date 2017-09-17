dc039ae52130a273d6267f0f6cc4fbe59771c5b1
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2017-09-17 10:12:27 +0600

    Create oddint.java

---
+++ oddint.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.scannar:
+class oddint
+{
+ public static void main(String args[])
+ {
+ int limit=100;
+ system.out.println("enter the odd numbers range from 1 and "+limit);
+ for(int i=1;i<=limit;i++)
+ {
+  if(i%2!=0)
+  {
+   system.out.println("the number is odd i"+"");
+   }
+   }
+   }
+   }
+ 
