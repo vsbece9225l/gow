5bbc92f380eb08eaf7b5e145b7c42fe6390d39e2
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2017-09-19 21:01:32 +0600

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.scannar;
+class reverse
+{
+ public static void main(string args[])
+ {
+   int n;
+   scannar s=new scannar(system.in);
+   n=s.nextint();
+   while(n!=0)
+   {
+    remainder=n%10;
+    reversee=reversee*10+remainder;
+    n/=10;
+    }
+    system.out.println("the reversed number is:"+reversee);
+    }
+    }
+    
+    
