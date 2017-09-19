43a879b9f41eeb91eed4b67d6212f52615c88ca2
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2017-09-19 20:42:59 +0600

    Create fibonacci.java

---
+++ fibonacci.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class fibonacci
+{
+ int n1=0,n2=1,n3,i,count=10;
+ system.out.println(n1+""+n2);
+ for(i=2;i<count;i++)
+ {
+  n3=n1+n2;
+  system.out.println(""+n3);
+  n1=n2;
+  n2=n3;
+  }
+ }
+ 
+ 
