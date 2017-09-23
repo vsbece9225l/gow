f26052238b7b902c1cef1626b402c8b82f4b140a
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2017-09-24 00:14:45 +0600

    Create gcd.c

---
+++ gcd.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+ int num1,num2,gcd,remainder;
+ int numerator,denominator;
+ scanf("%d%d",&num1,&num2);
+ if(num1>num2)
+ {
+  numerator=num1;
+  denominator=num2;
+  }
+  else
+  {
+   numerator=num2;
+   denominator=num1;
+   }
+   remainder=numerator%denominator;
+   while(remainder!=0)
+   {
+    numerator=denominator;
+    denominator=remainder;
+    remainder=numerator%denominator;
+    }
+    gcd=denominator;
+    printf("the gcd is%d",gcd);
+    }
