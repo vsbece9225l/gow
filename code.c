eaa1d3a4dab4223a631d6edd8884cfc5878d25e0
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2017-09-23 19:30:56 -0700

    Update code.c

--- code.c
+++ code.c
@@ -8,6 +8,7 @@ for(i=0;i<n;i++)
 {
  sum=sum+i;
  }
- printf("the sum of all numbers it has to be in lesser than the given number%d",sum);
+ int n1=sum+n;
+ printf("the sum of all numbers it has to be in lesser than the given number%d",n1);
  return 0;
  }
