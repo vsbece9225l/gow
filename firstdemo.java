c5feb347a57e528537825f016eeafd352bf86ae8
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2017-09-23 19:46:46 -0700

    Create firstdemo.java

---
+++ firstdemo.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class firstdemo
+{
+ public static void main(String [] args)
+ {
+   strig str="write neatly";
+   int len_cnt=lettercount(str);
+   system.out.println(len_cnt);
+   }
+   }
