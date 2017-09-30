53139d557c1f229d46e02eaae6ead6066283af76
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2017-09-30 12:35:58 +0600

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+ int n,fact=1;
+ printf("enter the number:\n");
+ scanf("%d",&n);
+ while(n>0>
+ {
+    fact=fact*n;
+    n--;
+    }
+    printf(the factorial of the given number is %d",fact);
+    return 0;
+    }
