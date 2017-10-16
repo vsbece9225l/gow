17e7473e2fb8c39905ad0813371b3573b3765e9b
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2017-10-15 20:09:44 -0700

    Create odd even array.c

---
+++ odd even array.c
@@ -0,0 +1,16 @@
+#iclude<stdio.h>
+void main()
+{
+ int a[3];
+ printf("enter the number:");
+ for(i=0;i<3;i++)
+ {
+  scanf("%d",&ai]);
+  if(a[i]%2==0)
+     printf("%d",a[i]*a[i]);
+     else
+     printf("%d",a[i]*a[i]*a[i]);
+     }
+     getch();
+     }
+     
