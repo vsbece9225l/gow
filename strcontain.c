0911025dc358982d1018727a25cd98ca4a3772fe
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2017-12-05 02:47:08 -0800

    Create strcontain.c

---
+++ strcontain.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+#define start m##a##i##n
+int start()
+{
+ char str[30];
+ strcpy(str,"i am a coder");
+ printf("the contained string is %s",str);
+ getch();
+ return 0;
+ }
