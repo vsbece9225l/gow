1a261bdf0e36bb7100101e89c87d158486aac12c
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2017-12-06 07:47:15 -0800

    Rename oddeven.c to oddeven6.c

---
+++ oddeven6.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+ int num;
+ printf("enter the number:");
+ scanf("%d",&num);
+ if((num&1)%==0)
+ {
+  printf("even %d",num);
+  }
+  else
+  {
+  printf("odd %d",num);
+  }
+  return 0;
+  }
