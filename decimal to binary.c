32db8e0f092437d5de3be827f90c821ca4d9bd81
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2017-12-08 06:35:57 -0800

    Create decimal to binary.c

---
+++ decimal to binary.c
@@ -0,0 +1,19 @@
+#inlude<stdio.h>
+int main()
+{
+ int a[10],i,j=3,n;
+ clrscr();
+ printf("enter the element:");
+ scanf("%d",&n);
+ for(i=0;n>0;i++)
+ {
+   a[i]=n%2;
+   n=n/2;
+   }
+   printf("\nbinary number is=");
+   for(j-1;j>=;J--)
+   {
+    printf("%d",a[i]);
+    }
+    return 0;
+    }
